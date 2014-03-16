98d5d03ffd67f56f27974a916f47db1f8b5f92b4
Blue Prawn <monnier.florent@gmail.com>
2014-03-16 17:14:53 +0059

    changed "-" to "_" in filename

--- Culture/Agenda/agenda_culturel.mk
+++ Culture/Agenda/agenda_culturel.mk
@@ -2,12 +2,12 @@ CAT = cat
 WGET = curl -s
 O_DATA = http://data.infolocale.fr/explore/dataset/agenda_culturel/download?format=csv
 
-all: agenda-culturel.csv
+all: agenda_culturel.csv
 
-.PHONY: agenda-culturel.csv
-agenda-culturel.csv:
+.PHONY: agenda_culturel.csv
+agenda_culturel.csv:
 	$(WGET) '$(O_DATA)' > $@
 
 clean:
-	$(RM) agenda-culturel.csv
+	$(RM) agenda_culturel.csv
 
