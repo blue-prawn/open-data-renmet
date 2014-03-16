ef071d9a1aeaf86b85d22d8aca08047bc98d1864
Blue Prawn <monnier.florent@gmail.com>
2014-03-16 18:44:54 +0059

    renamed to shorter filename

--- Culture/Statistiques/Frequentation/Champs_Libres/freq_champs_libres.mk
+++ Culture/Statistiques/Frequentation/Champs_Libres/freq_champs_libres.mk
@@ -11,50 +11,50 @@ O_DATA_H13 = http://www.data.rennes-metropole.fr/fileadmin/user_upload/data/bibl
 O_DATA_H14 = http://www.data.rennes-metropole.fr/fileadmin/user_upload/data/bibliotheques-de-rennes/2014/frequentation_parheure.csv
 
 all: \
-  frequentation_champs_libres_by_hour_2011.csv \
-  frequentation_champs_libres_by_hour_2012.csv \
-  frequentation_champs_libres_by_hour_2013.csv \
-  frequentation_champs_libres_by_hour_2014.csv \
-  frequentation_champs_libres_by_day_2011.csv \
-  frequentation_champs_libres_by_day_2012.csv \
-  frequentation_champs_libres_by_day_2013.csv \
+  freq_champs_libres_by_hour_2011.csv \
+  freq_champs_libres_by_hour_2012.csv \
+  freq_champs_libres_by_hour_2013.csv \
+  freq_champs_libres_by_hour_2014.csv \
+  freq_champs_libres_by_day_2011.csv \
+  freq_champs_libres_by_day_2012.csv \
+  freq_champs_libres_by_day_2013.csv \
   #End
 
 # current one
 
-frequentation_champs_libres_by_hour_2014.csv:
+freq_champs_libres_by_hour_2014.csv:
 	$(WGET) '$(O_DATA_H14)' > $@
 
 # by hour
 
-frequentation_champs_libres_by_hour_2011.csv:
+freq_champs_libres_by_hour_2011.csv:
 	$(WGET) '$(O_DATA_H11)' > $@
 
-frequentation_champs_libres_by_hour_2012.csv:
+freq_champs_libres_by_hour_2012.csv:
 	$(WGET) '$(O_DATA_H12)' > $@
 
-frequentation_champs_libres_by_hour_2013.csv:
+freq_champs_libres_by_hour_2013.csv:
 	$(WGET) '$(O_DATA_H13)' > $@
 
 # by day
 
-frequentation_champs_libres_by_day_2011.csv:
+freq_champs_libres_by_day_2011.csv:
 	$(WGET) '$(O_DATA_D11)' > $@
 
-frequentation_champs_libres_by_day_2012.csv:
+freq_champs_libres_by_day_2012.csv:
 	$(WGET) '$(O_DATA_D12)' > $@
 
-frequentation_champs_libres_by_day_2013.csv:
+freq_champs_libres_by_day_2013.csv:
 	$(WGET) '$(O_DATA_D13)' > $@
 
 # Hygiene
 
 clean:
-	$(RM) frequentation_champs_libres_by_hour_2011.csv
-	$(RM) frequentation_champs_libres_by_hour_2012.csv
-	$(RM) frequentation_champs_libres_by_hour_2013.csv
-	$(RM) frequentation_champs_libres_by_hour_2014.csv
-	$(RM) frequentation_champs_libres_by_day_2011.csv
-	$(RM) frequentation_champs_libres_by_day_2012.csv
-	$(RM) frequentation_champs_libres_by_day_2013.csv
+	$(RM) freq_champs_libres_by_hour_2011.csv
+	$(RM) freq_champs_libres_by_hour_2012.csv
+	$(RM) freq_champs_libres_by_hour_2013.csv
+	$(RM) freq_champs_libres_by_hour_2014.csv
+	$(RM) freq_champs_libres_by_day_2011.csv
+	$(RM) freq_champs_libres_by_day_2012.csv
+	$(RM) freq_champs_libres_by_day_2013.csv
 
