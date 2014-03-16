27f3742837302ba5315bd5760c6be3d68dd9fca8
Blue Prawn <monnier.florent@gmail.com>
2014-03-16 21:04:20 +0059

    make readable on small screen (segmented URI)

--- Culture/Statistiques/Frequentation/Champs_Libres/freq_champs_libres.mk
+++ Culture/Statistiques/Frequentation/Champs_Libres/freq_champs_libres.mk
@@ -1,14 +1,16 @@
 CAT = cat
 WGET = curl -s
 
-O_DATA_D11 = http://www.data.rennes-metropole.fr/fileadmin/user_upload/data/bibliotheques-de-rennes/frequentation2011_parjour.csv
-O_DATA_D12 = http://www.data.rennes-metropole.fr/fileadmin/user_upload/data/bibliotheques-de-rennes/frequentation2012_parjour.csv
-O_DATA_D13 = http://www.data.rennes-metropole.fr/fileadmin/user_upload/data/bibliotheques-de-rennes/frequentation2013_parjour.csv
+DATA_DIR = http://www.data.rennes-metropole.fr/fileadmin/user_upload/data
 
-O_DATA_H11 = http://www.data.rennes-metropole.fr/fileadmin/user_upload/data/bibliotheques-de-rennes/frequentation2011_parheure.csv
-O_DATA_H12 = http://www.data.rennes-metropole.fr/fileadmin/user_upload/data/bibliotheques-de-rennes/frequentation2012_parheure.csv
-O_DATA_H13 = http://www.data.rennes-metropole.fr/fileadmin/user_upload/data/bibliotheques-de-rennes/frequentation2013_parheure.csv
-O_DATA_H14 = http://www.data.rennes-metropole.fr/fileadmin/user_upload/data/bibliotheques-de-rennes/2014/frequentation_parheure.csv
+O_DATA_D11 = $(DATA_DIR)/bibliotheques-de-rennes/frequentation2011_parjour.csv
+O_DATA_D12 = $(DATA_DIR)/bibliotheques-de-rennes/frequentation2012_parjour.csv
+O_DATA_D13 = $(DATA_DIR)/bibliotheques-de-rennes/frequentation2013_parjour.csv
+
+O_DATA_H11 = $(DATA_DIR)/bibliotheques-de-rennes/frequentation2011_parheure.csv
+O_DATA_H12 = $(DATA_DIR)/bibliotheques-de-rennes/frequentation2012_parheure.csv
+O_DATA_H13 = $(DATA_DIR)/bibliotheques-de-rennes/frequentation2013_parheure.csv
+O_DATA_H14 = $(DATA_DIR)/bibliotheques-de-rennes/2014/frequentation_parheure.csv
 
 all: \
   freq_champs_libres_by_hour_2011.csv \
