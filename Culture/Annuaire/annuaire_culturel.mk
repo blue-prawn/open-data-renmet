701775cfe324f51fa7b58919155eeda55ffd4c59
Blue Prawn <monnier.florent@gmail.com>
2014-04-10 12:45:05 +0159

    main recipe "dl" to download

--- Culture/Annuaire/annuaire_culturel.mk
+++ Culture/Annuaire/annuaire_culturel.mk
@@ -2,7 +2,8 @@ CAT = cat
 CURL = curl -s
 ODATA =  http://www.data.rennes-metropole.fr/fileadmin/user_upload/data/arts_vivants/Export_RIC_Opendata_-_Arts_vivants.csv
 
-all: annuaire_culturel.csv
+.PHONY: dl
+dl: annuaire_culturel.csv
 
 .PHONY: annuaire_culturel.csv
 annuaire_culturel.csv:
