8e85d5d928b9a5098aa01db950d2ed8b482d1606
Blue Prawn <monnier.florent@gmail.com>
2014-04-10 12:25:09 +0159

    switch from wget to curl

--- Culture/Annuaire/annuaire_culturel.mk
+++ Culture/Annuaire/annuaire_culturel.mk
@@ -1,12 +1,12 @@
 CAT = cat
-WGET = curl -s
+CURL = curl -s
 ODATA =  http://www.data.rennes-metropole.fr/fileadmin/user_upload/data/arts_vivants/Export_RIC_Opendata_-_Arts_vivants.csv
 
 all: annuaire_culturel.csv
 
 .PHONY: annuaire_culturel.csv
 annuaire_culturel.csv:
-	$(WGET) '$(ODATA)' > $@
+	$(CURL) '$(ODATA)' > $@
 
 clean:
 	$(RM) annuaire_culturel.csv
